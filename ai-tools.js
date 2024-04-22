3a7c7ec9801d74739fe2b6f4aed8c62fcb95acdc
adougherty <adougherty@gmail.com>
2024-04-21 17:09:18 -0700

    Add Stable Diffussion 3 Support

--- ai-tools.js
+++ ai-tools.js
@@ -3,7 +3,14 @@ var aiToolsReady= false;
 Hooks.once('init', async function() {
     game.settings.register("ai-tools", "openaiApiKey", {
         name: "OpenAI API Key",
-        hint: "Get one from openai.com",
+        hint: "Get one from openai.com. This is required to generate images using OpenAI's DallE-3 model. As of April 21, 2024, A standard image costs $0.04 USD and an HD image costs $0.08 USD. (Charged by OpenAI)",
+        scope: "world",
+        config: true,
+        type: String,
+    });
+    game.settings.register("ai-tools", "sd3ApiKey", {
+        name: "Stable Diffusion 3 API Key",
+        hint: "Get one from platform.stability.ai. This is required to generate images using Stability AI's Stable Diffusion 3 model.",
         scope: "world",
         config: true,
         type: String,
@@ -15,16 +22,16 @@ Hooks.once('init', async function() {
         config: true,
         type: Boolean,
     });
+
     game.settings.register("ai-tools", "hd", {
-        name: "Generate HD Images",
+        name: "Generate HD Images (DallE-3)",
         hint: "HD images have higher quality and consistency, but cost more.",
         scope: "world",
         config: true,
         type: Boolean,
     });
-
     game.settings.register("ai-tools", "style", {
-        name: "Image Style:",
+        name: "Image Style (DallE-3):",
         hint: "The style of the generated images. Must be one of vivid or natural. Vivid causes the model to lean towards generating hyper-real and dramatic images. Natural causes the model to produce more natural, less hyper-real looking images",
         scope: "world",
         config: true,
@@ -36,6 +43,18 @@ Hooks.once('init', async function() {
         default: "vivid"
     });
 
+    game.settings.register("ai-tools", "service-token", {
+        name: "Service to use for tokens",
+        hint: "You can choose which service you wish to use for tokens. The default is OpenAI, but you can use other services in the future.",
+        scope: "world",
+        config: true,
+        type: String,
+        choices: {
+            "openai": "OpenAI",
+            "sd3": "Stable Diffusion 3"
+        },
+    });
+    
     console.log("AI-Tools Loaded")
     r = await FilePicker.createDirectory('data', 'ai-images');
 });
@@ -50,7 +69,15 @@ function uuidv4() {
 }
 
 function createRefreshImageButton(html) {
-    const portrait = html.find('.profile');
+    let portrait;
+    if (game.system.id=='dnd5e') {
+        portrait = html.find('.profile');
+    } else if (game.system.id=='pf2e') {
+        portrait = html.find('.profile-img');
+    } else {
+        console.error("AI-Tools: Unsupported system. Please report this to the developer.");
+        return [null, null];
+    }
     const portrait_parent = portrait.parent();
     portrait_parent.css('position', 'relative');
     const overlay = $('<div style="transition: background-color 4s ease; position:absolute;top:0px;left:0px;width:17px;height:auto;background-color:white;padding-top:2px;padding-left:2px" id="ai-tools-refresh-image"><i class="fa-solid fa-recycle"></i></div>');        
@@ -70,6 +97,15 @@ function getOpenAIPromptData(prompt) {
         "quality": quality,
         "style": style
     };    
+}
+
+function getSD3PromptData(prompt) {
+    let formData = new FormData();
+    formData.append('prompt', prompt);
+    formData.append('output_format', 'webp');
+    formData.append('style-preset', 'fantasy-art');
+    formData.append('model', 'sd3-turbo');
+    return formData;
 }
 
 function startInProgress() {
@@ -135,8 +171,16 @@ Hooks.on('renderItemSheet', (sheet, html) => {
 });
 
 Hooks.on('renderActorSheet', (sheet, html) => {
+    drawActorSheetOverlay(sheet, html);
+});
+
+Hooks.on('renderNPCSheetPF2e', (sheet, html) => {
+    drawActorSheetOverlay(sheet, html);
+});
+
+function drawActorSheetOverlay(sheet, html) {
     if (!game.user.isGM) return;
-    
+
     if (sheet.actor.type === 'npc') {
         var overlay = createRefreshImageButton(html)[0];
 
@@ -146,7 +190,7 @@ Hooks.on('renderActorSheet', (sheet, html) => {
                 return;
             }
             var actor = sheet.actor;
-            var prompt = "";
+            let prompt = `A highly detailed and realistic portrait of a ${actor.name}, a ${actor.system.details.type.value} in a fantasy world. It should be depicted standing in its natural environment`;
             if (actor.flags.aitoolsPrompt) {
                 prompt = actor.flags.aitoolsPrompt;
             } else {
@@ -178,52 +222,65 @@ Hooks.on('renderActorSheet', (sheet, html) => {
         tab_biography.append($('<h2 style="font-weight:bold;flex: 0 0 auto">AI Image Creation Instructions</h2>'));
         tab_biography.append($(`<div style="font-weight:bold;flex: 0 0 auto"><input type="text" name="flags.aitoolsPrompt" style="width:100%" value="${sheet.actor.flags.aitoolsPrompt || ''}" id="aitoolsPrompt" placeholder="A fierce half-orc tracker carrying a magical notebook"></div>`));
     }
-});
+}
 
-function getNewPortrait(prompt, docUpdateCallBack = () => {}, retry = 0, options = {}) {
-    var nid = ui.notifications.info("Generating new portrait. Please be patient, this could take a few seconds. You will be notified when the image is ready.", {permanent: true});
-
-    console.log(prompt);
-
-    const data = getOpenAIPromptData(prompt);
-    if (options.style) data.style = options.style;
-    if (options.quality) data.quality = options.quality;
-    console.log(options)
-    console.log(data);
-
-    var interval = startInProgress();
-    
-    fetch("https://api.openai.com/v1/images/generations", {
+function fetchFromOpenAI(data, docUpdateCallBack, nid, retry) {
+    console.log('HERE OPENAI')
+    return fetch("https://api.openai.com/v1/images/generations", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${game.settings.get("ai-tools", "openaiApiKey")}`
         },
         body: JSON.stringify(data)
-        })
-        .then(response => {
-            ui.notifications.remove(nid);
-            if (!response.ok) {
-                ui.notifications.error("Failed to contact OpenAI servers. Please try again later.");
-                return;
-                if (retry < 3) {
-                    ui.notifications.warn("Failed to contact OpenAI severs. Trying again...");
-                    getNewPortrait(prompt, docUpdateCallBack, retry + 1);
-                } else {
-                    ui.notifications.error("Failed to contact OpenAI servers. Please try again later.");
-                }
+    });
+}
+
+function fetchFromSD3(data, docUpdateCallBack, nid, retry) {
+    return fetch("https://api.stability.ai/v2beta/stable-image/generate/core", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Authorization": `Bearer ${game.settings.get("ai-tools", "sd3ApiKey")}`
+        },
+        body: data
+    });
+}
+
+function getNewPortrait(prompt, docUpdateCallBack = () => {}, retry = 0, options = {}) {
+    options.type ||= 'portrait';
+    var nid = ui.notifications.info("Generating new portrait. Please be patient, this could take a few seconds. You will be notified when the image is ready.", {permanent: true});
+
+    console.log(prompt);
+
+    const data = (options.type=='token' && game.settings.get("ai-tools", "service-token") === 'sd3') ? getSD3PromptData(prompt) : getOpenAIPromptData(prompt);
+    var interval = startInProgress();
+    console.log(game.settings.get("ai-tools", "service-token"));
+    let promise = (options.type=='token' && game.settings.get("ai-tools", "service-token") === 'sd3') ? fetchFromSD3(data, docUpdateCallBack, nid, retry) : fetchFromOpenAI(data, docUpdateCallBack, nid, retry);
+    promise.then(response => {
+        ui.notifications.remove(nid);
+        if (!response.ok) {
+            ui.notifications.error("Failed to contact image servers. Please try again later.");
+            return;
+            if (retry < 3) {
+                ui.notifications.warn("Failed to contact OpenAI severs. Trying again...");
+                getNewPortrait(prompt, docUpdateCallBack, retry + 1);
             } else {
-                ui.notifications.info("Image generated.");
+                ui.notifications.error("Failed to contact OpenAI servers. Please try again later.");
             }
-            return response.json();
-        })
-        .then(async (data) => {
-            const b64 = "data:image/webp;base64," + data.data[0].b64_json
-            console.log(data.data[0]);
-            docUpdateCallBack(b64);
-            clearInterval(interval);
-        })
-        .catch(error => console.error('Error:', error));
+        } else {
+            ui.notifications.info("Image generated.");
+        }
+        return response.json();
+    })
+    .then(async (data) => {
+        console.log(data);
+        const b64 = "data:image/webp;base64," + (options.type=='token' && game.settings.get("ai-tools", "service-token") === 'sd3' ? data.image : data.data[0].b64_json)
+        console.log(b64);
+        docUpdateCallBack(b64);
+        clearInterval(interval);
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 
@@ -262,8 +319,11 @@ function base64ImageToBlob(str) {
 
 function generateTokenImage(token) {
     console.log(token);
-    if (!game.settings.get("ai-tools", "openaiApiKey")) {
+    if (game.settings.get("ai-tools", "service-token") == "openai" && !game.settings.get("ai-tools", "openaiApiKey")) {
         ui.notifications.error("Please set your OpenAI API key in the settings.");
+        return;
+    } else if (game.settings.get("ai-tools", "service-token") == "sd3" && !game.settings.get("ai-tools", "sd3ApiKey")) {
+        ui.notifications.error("Please set your Stable Diffusion 3 API key in the settings.");
         return;
     }
 
@@ -275,8 +335,11 @@ function generateTokenImage(token) {
         prompt += "\n\n The following is a more accurate description of what should be inside the ring of the token: " + actor.flags.aitoolsPrompt;
     }
 
-    let bio = actor.system.details.biography.value.replace(/<[^>]*>?/gm, '');
-    if (bio.length > 0) prompt += "\n\nThe following was provided as the biography of the NPC. You can ignore the mechanics, but draw inspiration of what the character might look like based on the description:\n" + bio
+    if (game.settings.get("ai-tools", "service-token") === 'openai') {
+        let bio = actor.system.details.biography.value.replace(/<[^>]*>?/gm, '');
+        if (bio.length > 0) prompt += "\n\nThe following was provided as the biography of the NPC. You can ignore the mechanics, but draw inspiration of what the character might look like based on the description:\n" + bio
+    }
+
     getNewPortrait(prompt, async (b64) => {
         const random_uuid = getUUID();
         await ImageHelper.uploadBase64(b64, `${random_uuid}.webp`, 'ai-images')
@@ -285,7 +348,7 @@ function generateTokenImage(token) {
             'texture.src': `ai-images/${random_uuid}.webp`,
             'flags.aiToolsImage': true
         })
-    });
+    }, 0, {type:'token'});
 }
 
 Hooks.on('renderTokenConfig', (app, html) => {
@@ -312,5 +375,4 @@ Hooks.on('drawToken', (token) => {
     if (!aiToolsReady) return;
     if (!game.settings.get("ai-tools", "generateNewTokens")) return;
     generateTokenImage(token.document);
-
 });
